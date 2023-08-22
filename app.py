2630c40a2fe6c020e351521e1152ee8c70df6295
ui-fankfurt <ui.frankfurt@gmail.com>
2023-08-22 12:21:42 +0800

    adding new files

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", 'rb'))
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+    json_ = request.json
+    query_df = pd.DataFrame(json_)
+    prediction = model.predict(query_df)
+    return jsonify({"Prediction":list(prediction)})
+
+if __name__ == "__main__":
+    app.run(debug = True)
